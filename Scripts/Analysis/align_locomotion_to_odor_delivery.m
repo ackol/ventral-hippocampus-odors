0ab04215cd8afedd2a4650f0917ca3df44aa3b74
Anna C. Kolstad <22373026+ackol@users.noreply.github.com>
2026-04-14 18:06:42 -0400

    Build out functionality to evaluate effect of odor delivery on running behavior. Work in progress: implement new statistical methods for evaluating neural responses to odors. Also update plotting of speed tuned neurons.

---
+++ Scripts/Analysis/align_locomotion_to_odor_delivery.m
@@ -0,0 +1,415 @@
+% align_sorted_spikes_to_odor_delivery.m - This code is designed to import 
+% data already processed from a single full recording after spike sorting (with
+% manual curation and anatomical alignment already applied) and align the 
+% spiking activity with odor delivery.
+%
+%   Inputs:
+%       (1) [IDxxx]_full_session_odor_sequence_matrix.mat (containing
+%       "odorSeqMat" variable and others)
+%       
+%       (2) [IDxxx]_D[x]_full_session_pid_data.mat (containing "odorSetInfo" variable)
+%       
+%       (3) [IDxxx]_paired_full_session_inst_velocity.mat (containing
+%       "???" variable)
+%
+%
+%   Outputs:
+%       (1) [IDxxx]_locomotion_grouped_by_odor_trial.mat, containing the
+%       following variables:
+%           locomotionMatrix
+%           mouseLabel
+%           timeBefore
+%           timeDuring
+%           timeAfter
+%           sampFreq
+%           nOdors
+%           nTrialsPerOdor
+%           odorIdentities
+%       
+%
+%   Dependencies:
+%       ...
+%
+% Designed by Anna C. Kolstad
+% Author contact email: anna_kolstad@urmc.rochester.edu
+% Padmanabhan Lab, University of Rochester School of Medicine
+% PI contact email: krishnan_padmanabhan@urmc.rochester.edu
+% Script first created: April 13, 2026
+% Script last updated: April 13, 2026
+% Note: This script is adapted from align_sorted_spikes_to_odor_delivery.m
+% Version 1.0
+
+%% PART ONE: Load data and get parameters
+
+clear vars
+clc
+
+% Manually specify Mouse ID
+mouseLabel = inputdlg('Enter animal ID','User input');
+
+% select base directory from which to navigate
+baseDir = uigetdir('',"Select base directory for this animal.");
+
+% load odor delivery timing
+[file, path] = uigetfile(".mat","Select [IDxxx]_full_session_odor_sequence_matrix.mat",baseDir);
+load(fullfile(path,file));
+
+% load odorant identities
+[file, path] = uigetfile(".mat","Select .mat file containing PID data ([AK0xx]_D[x]_full_session_pid_data.mat.",path);
+load(fullfile(path,file),"odorSetInfo");
+thisOdorSet = odorSetInfo.setLayout;
+if length(thisOdorSet)==1
+    thisOdorSetInfo = odorSetInfo.("Set_" + thisOdorSet);
+else
+    error("Need to adapt the code to handle more than one sequentially presented odor set!")
+end
+odorIdentities = cellstr(thisOdorSetInfo.odorNames);
+
+% load alignedUnitDataStruct variable
+[file, path] = uigetfile(".mat","Select _paired_full_session_inst_velocity.mat variable for " + mouseLabel + ".",baseDir);
+tic
+disp("Loading velocity structure...")
+load(fullfile(path,file));
+toc            
+
+% get paths for output figures
+baseOutputPath = uigetdir(baseDir,"Select base directory in which to save outputs.");
+
+% Infer experimental parameters
+nOdors = length(odorIdentities);
+nTrialsPerOdor = length(odorSeqMat)/nOdors; % Here, this calculation rests upon the assumption that all odors are presented an equal number of times. May not always be true.
+
+odorSampFreq = 30000; % Hz % TO-DO - in process_raw_behavior_rig_data, need to save/export sample frequency for odor data
+odorTimeVector =  1:(1/odorSampFreq):((length(odorSignal)-1)/odorSampFreq); % in sec. 
+nOdorPresentations = size(odorSeqMat,1); % total number of odorant trials in this experiment
+onPoints = odorSeqMat(:,4); % sampling points at which odor turned on
+offPoints = odorSeqMat(:,5); % sampling points at which odor turned off
+odorDeliveryDurations = offPoints - onPoints; % Observation: there is some error in the on/off timing for each odorant trial relative to other trials
+odorDeliveryDurationsTime = odorDeliveryDurations/odorSampFreq; % Observation: that error is all in the thousandths of a second range (millisecond error)
+onTimes = odorSeqMat(:,4)/odorSampFreq; % sample points at which odor turned on, in seconds
+offTimes = odorSeqMat(:,5)/odorSampFreq; % sample points at which odor turned off, in seconds
+
+% Define intra-trial time windows
+timeBefore = 5; % (seconds) set the amount of time prior to odorant onset to be considered "before" time
+timeDuring = 3; % (seconds) set duration of odorant presentation
+timeAfter = 10; % (seconds) set the amount of time after odorant onset to be considered "after" time
+pointsBefore = timeBefore*odorSampFreq; 
+pointsDuring = timeDuring*odorSampFreq;
+pointsAfter = timeAfter*odorSampFreq;
+
+singleTrialTimeVector = -timeBefore:(1/odorSampFreq):(timeDuring+timeAfter-1/odorSampFreq); % in seconds
+
+% Get total recording duration (in sample points)
+nSamples = length(wheelVelocity);
+
+clear file
+
+disp("Finished loading data and getting parameters. PART ONE complete.")
+
+%% PART TWO: Group spikes by odor trial and save to .mat file
+
+% GOT ONLY TO HERE WITH REVISIONS!!
+clear trialCountByOdor odorantNum onPoint
+
+% Preallocate memory for new data structure
+runningRaster = nan(nOdors, nTrialsPerOdor, 1, pointsBefore+pointsDuring+pointsAfter); % Create empty 3D matrix for storing per-trial running traces, grouped by odorant identity
+
+% Populate new data structure, organizing locomotion trace by odor identity,
+% trial number
+tic
+disp("Grouping running data by trial...")
+trialCountByOdor = zeros(1,nOdors);
+for iPresentation = 1:nOdorPresentations
+    fprintf('Processing presentation #%d/%d \r',iPresentation,nOdorPresentations);
+    drawnow;
+    odorantNum = odorSeqMat(iPresentation,1); % get port number
+    onPoint = onPoints(iPresentation);
+    trialCountByOdor(1,odorantNum) = trialCountByOdor(1,odorantNum) + 1; % increment trial count for that odorant
+    
+    runningRaster(odorantNum, trialCountByOdor(1,odorantNum), :, :) = wheelVelocity(:, (onPoint-pointsBefore):(onPoint+pointsDuring+pointsAfter-1));
+end
+fprintf('\n')
+toc
+%%
+clear iPresentation odorantNum 
+
+tic
+disp("Saving locomotion_grouped_by_odor_trial data structure to .mat file.")
+save(baseOutputPath + "\" + mouseLabel + "" + "_locomotion_grouped_by_odor_trial.mat","runningRaster", "mouseLabel","timeBefore","timeDuring","timeAfter","sampFreq","nOdors","nTrialsPerOdor","odorIdentities",'-v7.3');
+toc
+
+disp("Finished grouping spikes by odor trial and saving to file. PART TWO complete.")
+
+disp("Done running script.")
+
+%% NEW VERSION OF SCRIPT ENDS HERE. IF THE BELOW PLOTS ARE DESIRED, THEY 
+% SHOULD BE BROKEN OUT INTO SEPARATE PLOTTING AND ANALYSIS FUNCTIONS AS WAS
+% DONE WITH "plot_raw_spike_counts_before_vs_during_odor_delivery.m", 
+% "compare_spike_counts_before_vs_during_with_WSR.m", 
+% "plot_spike_rasters_for_all_odorant_trials.m", and
+% "plot_PSTHs_for_all_odorants.m".
+
+
+% savedirAbsoluteChange = baseOutputPath + "\" + "4 - absolute change in spike counts";
+% savedirAbsoluteChangeBar = baseOutputPath + "\" + "5 - spike count bar graph";
+% savedirPercentChange = baseOutputPath + "\" + "6 - percent change";
+% savedirPercentChangeBar = baseOutputPath + "\" + "7 - percent change bar";
+% 
+% % make new folders if they do not already exist
+% if ~exist(savedirAbsoluteChange,'dir')
+%     mkdir(savedirAbsoluteChange);
+% end
+% if ~exist(savedirAbsoluteChangeBar,'dir')
+%     mkdir(savedirAbsoluteChangeBar);
+% end
+% if ~exist(savedirPercentChange,'dir')
+%     mkdir(savedirPercentChange);
+% end
+% if ~exist(savedirPercentChangeBar,'dir')
+%     mkdir(savedirPercentChangeBar);
+% end
+% savedirStatisticalTests = baseOutputPath + "\" + "8 - statistical tests";
+% if ~exist(savedirStatisticalTests,'dir')
+%     mkdir(savedirStatisticalTests);
+% end
+
+% %% PART THREE: Compute spike counts before and during odor delivery
+% 
+% disp("Computing spike counts before and during odor delivery.")
+% 
+% tic
+% 
+% % Preallocate memory for new data structure
+% beforeCount = nan(nOdors, nTrialsPerOdor, nUnits); % Create empty 3D matrix for storing total count of spikes per trial, grouped by odorant identity
+% duringCount = nan(nOdors, nTrialsPerOdor, nUnits); % Create empty 3D matrix for storing total count of spikes per trial, grouped by odorant identity
+% 
+% startIndex = pointsBefore-pointsDuring; % start counting spikes from 2 seconds in to trial, 
+% onsetIndex = startIndex + pointsDuring;
+% endIndex = onsetIndex + pointsDuring;
+% 
+% for iUnit = 1:nUnits
+% 
+%     disp("Processing unit #" + num2str(iUnit))
+% 
+%     for iOdorant = 1:nOdors
+% 
+%         for iTrial = 1:nTrialsPerOdor
+%             beforeCount(iOdorant, iTrial, iUnit) = sum(spikeRaster(iOdorant,iTrial,iUnit,startIndex:(onsetIndex-1)));
+%             duringCount(iOdorant, iTrial, iUnit) = sum(spikeRaster(iOdorant,iTrial,iUnit,onsetIndex:endIndex));
+%         end
+% 
+%     end
+% end
+% 
+% disp("Finished computing spike counts. PART THREE complete.")
+% 
+% %% PART FOUR: Plotting
+% 
+% nRows = 4;
+% 
+% 
+% %% PART 4.4: Plot absolute change in spike count during odor delivery
+% 
+% disp("Plotting absolute change in spike count upon odor delivery...")
+% 
+% clear duringMetric
+% 
+% absoluteChangeMean = nan(nUnits,nOdors);
+% absoluteChangeStd = nan(nUnits,nOdors);
+% 
+% signTestPValues = nan(nUnits,nOdors);
+% 
+% for iUnit = 1:nUnits
+% 
+%     disp("Processing unit #" + num2str(iUnit))
+% 
+%     thisContactNum = unitInfo.("UNIT" + num2str(iUnit, "%.3d")).info.contactNumb;  %getContactNumber(iUnit);
+%     thisAnatomicalRegion = unitInfo.("UNIT" + num2str(iUnit, "%.3d")).info.anatomicAbbrev;
+%     f = figure('visible','off');
+%     sgtitle({"Mouse " + mouseLabel, "sorted & curated", "Unit #" + num2str(iUnit), "Ch#" + thisContactNum + ", " + thisAnatomicalRegion})
+%     set(gcf,'Position',[550 50 700 1300])
+% 
+%     minY = 0;
+%     maxY = 0;
+%     for iOdorant = 1:nOdors
+% 
+%         subplot(nRows,nOdors/nRows,iOdorant)
+% 
+%         countDiff = (duringCount(iOdorant, :, iUnit)-beforeCount(iOdorant, :, iUnit))./timeDuring; % in spikes/sec
+%         x = [1 2];
+% 
+%         for iTrial = 1:nTrialsPerOdor
+%             y = [0 countDiff(1,iTrial)];
+%             plot(x,y,'-k.')
+%             hold on
+%         end
+% 
+%         % get bounds for plotting
+%         maxY = max(countDiff(isfinite(countDiff)));
+%         minY = min(countDiff(isfinite(countDiff)));
+% 
+%         % Calculate statistical characteristics of the data
+%         absoluteChangeMean(iUnit,iOdorant) = mean(countDiff(isfinite(countDiff)));
+%         absoluteChangeStd(iUnit,iOdorant) = std(countDiff(isfinite(countDiff)));
+%         % Perform statistical test to evaluate whether this unit's responds
+%         % to this odorant:
+%         % Sign Test -- evaluates whether the median of the paired sample
+%         % differences is zero
+%         signTestPValues(iUnit,iOdorant) = signtest(countDiff);
+% 
+%         % Set plotting parameters
+%         xlim([0.5 2.5])
+%         if minY==maxY
+%             ylim([-10 10])
+%         else
+%             ylim([minY*1.2 maxY*1.2])
+%         end
+%         xlabel({'time period relative to', 'odorant delivery'})
+%         ylabel('absolute change in spike count (spikes/sec)')
+%         xticks([1 2])
+%         xticklabels({'before','during'})
+%         yline(0,'--')
+%         title({odorIdentities{iOdorant}, "sign test p-value: " + sprintf('%.3f',signTestPValues(iUnit,iOdorant))})
+%         hold off
+% 
+%     end
+% 
+%     if saveImages == true
+%         saveas(f, savedirAbsoluteChange + "\" + mouseLabel + "_unit-" + num2str(iUnit, "%.3d") + "_beforeduring_absolutechange.png")
+%     end
+% end
+% 
+% disp("Finished plotting absolute change in spike counts.")
+% 
+% %% PART 4.5: Plot bar graph of absolute change in spike count before and after delivery
+% 
+% if saveImages == true
+%     for iUnit = 1:nUnits
+%         disp("Processing unit #" + num2str(iUnit))
+% 
+%         thisContactNum = unitInfo.("UNIT" + num2str(iUnit, "%.3d")).info.contactNumb;
+%         thisAnatomicalRegion = unitInfo.("UNIT" + num2str(iUnit, "%.3d")).info.anatomicAbbrev;
+%         f = figure('visible','off');
+%         sgtitle("Mouse " + mouseLabel + " - sorted & curated - Unit #" + num2str(iUnit) + ", Ch#" + thisContactNum + ", " + thisAnatomicalRegion)
+%         set(gcf,'Position',[550 550 700 500])
+% 
+%         x = 1:nOdors;
+%         bar(x,absoluteChangeMean(iUnit,:))                
+%         hold on
+%         er = errorbar(x,absoluteChangeMean(iUnit,:),absoluteChangeStd(iUnit,:));    
+%         er.Color = [0 0 0];                            
+%         er.LineStyle = 'none';  
+%         xticks(x)
+%         xticklabels(odorIdentities)
+%         xlabel('odorant')
+%         ylabel({'absolute change in spike count','(error bars 2SD in length)'})
+%         hold off
+% 
+%         saveas(f, savedirAbsoluteChangeBar + "\" + mouseLabel + "_unit-" + num2str(iUnit, "%.3d") + "_beforeduring_absolutechange_bargraph-mean-std.png")
+% 
+%     end
+% 
+%     disp("Finished plotting bar graph of absolute change in spike counts.")
+% end
+% 
+% %% PART 4.6: Plot percent change in spike count before and during odor delivery
+% 
+% disp("Plotting percent change in spike count before vs. during odor delivery...")
+% 
+% clear duringMetric
+% 
+% meanPercentChange = nan(nUnits,nOdors);
+% 
+% for iUnit = 1:nUnits
+% 
+%     disp("Processing unit #" + num2str(iUnit))
+% 
+%     thisContactNum = unitInfo.("UNIT" + num2str(iUnit, "%.3d")).info.contactNumb;  %getContactNumber(iUnit);
+%     thisAnatomicalRegion = unitInfo.("UNIT" + num2str(iUnit, "%.3d")).info.anatomicAbbrev;
+%     f = figure('visible','off');
+%     sgtitle({"Mouse " + mouseLabel, "sorted & curated", "Unit #" + num2str(iUnit), "Ch#" + thisContactNum + ", " + thisAnatomicalRegion})
+%     set(gcf,'Position',[550 50 700 1300])
+% 
+% 
+%     minY = 0;
+%     maxY = 0;
+%     for iOdorant = 1:nOdors
+% 
+%         subplot(nRows,nOdors/nRows,iOdorant)
+% 
+%         duringMetric = (duringCount(iOdorant, :, iUnit)-beforeCount(iOdorant, :, iUnit))./beforeCount(iOdorant, :, iUnit)*100;
+%         x = [1 2];
+% 
+%         for iTrial = 1:nTrialsPerOdor            
+%             y = [0 duringMetric(1,iTrial)];
+%             plot(x,y,'-k.')
+%             hold on
+%         end
+% 
+%         maxY = max(duringMetric(isfinite(duringMetric)));
+%         minY = min(duringMetric(isfinite(duringMetric)));
+%         xlim([0.5 2.5])
+%         if isempty(minY)
+%             ylim([-1 1])
+%         elseif minY == maxY
+%             ylim([minY-1 maxY+1])
+%         else
+%             ylim([minY*1.2 maxY*1.2])
+%         end
+%         xlabel({'time period relative to', 'odorant delivery'})
+%         ylabel('% change in spike count')
+%         xticks([1 2])
+%         xticklabels({'before','during'})
+%         yline(0,'--')
+%         title(odorIdentities{iOdorant})
+%         hold off
+% 
+%         meanPercentChange(iUnit,iOdorant) = mean(duringMetric(isfinite(duringMetric)));
+%         percentChangeStd(iUnit,iOdorant) = std(duringMetric(isfinite(duringMetric)));
+% 
+%     end
+% 
+%     if saveImages == true
+%         saveas(f, savedirPercentChange + "\" + mouseLabel + "_unit-" + num2str(iUnit, "%.3d") + "_beforeduring_percentchange.png")
+%     end
+% end
+% 
+% disp("Finished plotting percent change in spike counts.")
+% 
+% %% PART 4.7: Plot bar graph of percent change in spike count before and after delivery
+% 
+% if saveImages == true
+%     disp("Plotting bar graph of percent change in spike count...")
+% 
+%     for iUnit = 1:nUnits
+% 
+%         disp("Processing unit #" + num2str(iUnit))
+% 
+%         thisContactNum = unitInfo.("UNIT" + num2str(iUnit, "%.3d")).info.contactNumb;  %getContactNumber(iUnit);
+%         thisAnatomicalRegion = unitInfo.("UNIT" + num2str(iUnit, "%.3d")).info.anatomicAbbrev;
+%         f = figure('visible','off');
+%         sgtitle("Mouse " + mouseLabel + " - sorted & curated - Unit #" + num2str(iUnit) + ", Ch#" + thisContactNum + ", " + thisAnatomicalRegion)
+%         set(gcf,'Position',[550 550 700 500])
+% 
+%         x = 1:nOdors;
+%         bar(x,meanPercentChange(iUnit,:))                
+%         hold on
+%         er = errorbar(x,meanPercentChange(iUnit,:),percentChangeStd(iUnit,:));    
+%         er.Color = [0 0 0];                            
+%         er.LineStyle = 'none';  
+%         xticks(x)
+%         xticklabels(odorIdentities)
+%         xlabel('odorant')
+%         ylabel({'percent change in spike count','(error bars 2SD in length)'})
+%         hold off
+% 
+%         saveas(f, savedirPercentChangeBar + "\" + mouseLabel + "_unit-" + num2str(iUnit, "%.3d") + "_beforeduring_percentchange_bargraph-mean-std.png")
+% 
+%     end
+% 
+%     disp("Finished plotting bar graph of percent change in spike count.")
+% end
+% 
+% 
+
+
---
+++ Scripts/Analysis/compare_locomotion_and_odor_tuning.m
@@ -0,0 +1,98 @@
+% compare_locomotion_and_odor_tuning.m - This 
+% script saves ... 
+%
+%   Inputs:
+%       USER MUST SPECIFY VIA UI:
+%       (1) allSpeedScoreInfo.mat
+%       (2) significantResultsAllMice.mat
+%       (3) allUnitInfo.mat
+%   Outputs:
+%       (1) display in the command window the fraction of units that are
+%       tuned to both speed and odors
+%
+%   Dependencies: none
+%
+% Designed by Anna C. Kolstad
+% Author contact email: anna_kolstad@urmc.rochester.edu
+% Padmanabhan Lab, University of Rochester School of Medicine
+% PI contact email: krishnan_padmanabhan@urmc.rochester.edu
+% Script first created: April 13, 2026
+% Script last updated: April 13, 2026
+% Version 1.0.
+
+%% PART 1: Load in Data
+
+% Load in tested speed scores
+[file, path] = uigetfile(".mat","Select allSpeedScoreInfo.mat file for all mice.");
+disp("Loading tested speed scores...")
+load(fullfile(path,file)); 
+
+% Load in WSR significant odor tunings
+[file, path] = uigetfile(".mat","Select significantResultsAllMice.mat file.");
+disp("Loading odor tuned neurons...")
+load(fullfile(path,file)); 
+nTunedOdorUnitPairs = height(significantResults);
+
+% Load in allUnitInfo
+[file, path] = uigetfile(".mat","Select allUnitInfo.mat file.");
+disp("Loading unit information...")
+load(fullfile(path,file)); 
+
+%% PART 2: Assess overlap
+
+% Create lookup table for region based on (mouse ID + unit ID)
+allUnits = testedSpeedScores.Mouse + "-" + testedSpeedScores.("Unit #");
+assignedRegions = testedSpeedScores.("Anatomical Abbreviation");
+allUnits = [allUnits assignedRegions];
+
+% Get list of odor tuned units (mouse ID + unit ID)
+odorTunedUnits = significantResults.Mouse + "-" + significantResults.("Unit #");
+odorTunedUnits = unique(odorTunedUnits);
+nOdorTunedUnits = height(odorTunedUnits);
+% Append region
+for i = 1:nOdorTunedUnits
+    odorTunedUnits(i,2) = allUnits(find(strcmp(allUnits(:,1),odorTunedUnits(i,1))),2);
+end
+
+% Get list of speed tuned units (mouse ID + unit ID)
+speedTunedUnitsTable = [testedSpeedScores(testedSpeedScores.Classification == "positive speed cell",:); testedSpeedScores(testedSpeedScores.Classification == "negative speed cell",:)];
+speedTunedUnits = speedTunedUnitsTable.Mouse + "-" + speedTunedUnitsTable.("Unit #");
+nSpeedTunedUnits = height(speedTunedUnits);
+% Append region
+for i = 1:nSpeedTunedUnits
+    speedTunedUnits(i,2) = allUnits(find(strcmp(allUnits(:,1),speedTunedUnits(i,1))),2);
+end
+
+% Assess what fraction of speed tuned cells also respond to odors
+overlap = intersect(odorTunedUnits(:,1),speedTunedUnits(:,1));
+nBoth = length(overlap); % List of units that are tuned to both odor and speed
+% Append region
+for i = 1:nBoth
+    overlap(i,2) = allUnits(find(strcmp(allUnits(:,1),overlap(i,1))),2);
+end
+
+disp("Fraction of odor tuned neurons that are speed tuned: " + nBoth/nOdorTunedUnits)
+disp("Fraction of speed tuned neurons that are odor tuned: " + nBoth/nSpeedTunedUnits)
+
+% Assess CA1 only
+odorTunedUnitsCA1 = [odorTunedUnits(strcmp(odorTunedUnits(:,2),"vCA1"),:); odorTunedUnits(strcmp(odorTunedUnits(:,2),"iCA1"),:)];
+nOdorTunedUnitsCA1 = length(odorTunedUnitsCA1);
+speedTunedUnitsCA1 = [speedTunedUnits(strcmp(speedTunedUnits(:,2),"vCA1"),:); speedTunedUnits(strcmp(speedTunedUnits(:,2),"iCA1"),:)];
+nSpeedTunedUnitsCA1 = length(speedTunedUnitsCA1);
+overlapCA1 = [overlap(strcmp(overlap(:,2),"vCA1"),:); overlap(strcmp(overlap(:,2),"iCA1"),:)];
+nBothCA1 = length(overlapCA1);
+
+disp("Fraction of odor tuned CA1 neurons that are speed tuned: " + nBothCA1/nOdorTunedUnitsCA1)
+disp("Fraction of speed tuned CA1 neurons that are odor tuned: " + nBothCA1/nSpeedTunedUnitsCA1)
+
+
+% % Assess CA3 only -- not enough speed tuned neurons to be meaningful
+% odorTunedUnitsCA3 = [odorTunedUnits(strcmp(odorTunedUnits(:,2),"vCA3"),:); odorTunedUnits(strcmp(odorTunedUnits(:,2),"iCA3"),:)];
+% nOdorTunedUnitsCA3 = length(odorTunedUnitsCA3);
+% speedTunedUnitsCA3 = [speedTunedUnits(strcmp(speedTunedUnits(:,2),"vCA3"),:); speedTunedUnits(strcmp(speedTunedUnits(:,2),"iCA3"),:)];
+% nSpeedTunedUnitsCA3 = length(speedTunedUnitsCA3);
+% overlapCA3 = [overlap(strcmp(overlap(:,2),"vCA3"),:); overlap(strcmp(overlap(:,2),"iCA3"),:)];
+% nBothCA3 = length(overlapCA3);
+% 
+% disp("Fraction of odor tuned CA3 neurons that are speed tuned: " + nBothCA3/nOdorTunedUnitsCA3)
+% disp("Fraction of speed tuned CA3 neurons that are odor tuned: " + nBothCA3/nSpeedTunedUnitsCA3)
